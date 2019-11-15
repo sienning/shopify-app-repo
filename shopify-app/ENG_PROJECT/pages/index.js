b54cc04431a45fbf951486e892fcdc648fa7e251
Léna ENG <55486985+sienning@users.noreply.github.com>
2019-11-15 09:45:16 +0100

    Ajout dossier shopify-app

---
+++ shopify-app/ENG_PROJECT/pages/index.js
@@ -0,0 +1,18 @@
+import { Layout, Page } from "@shopify/polaris";
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
+
+class Index extends React.Component {
+  render() {
+    return (
+      <Page>
+        <Layout>
+          
+        </Layout>
+      </Page >
+    );
+  }
+}
+
+export default Index;
